0579d0f0778bca23896baa8164b5c694d7edd4e7
Jonathan Yeung <yeung@guppy.co.jp>
2024-09-11 18:33:25 +0900

    feature: click on tile to open browser and slight change of ui

--- lib/screen/data_view.dart
+++ lib/screen/data_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sample_2024/model/response.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DataView extends StatelessWidget {
   const DataView({super.key, required this.response});
 
   final GithubResponse response;
+
+  void launch(Uri url) async {
+    await launchUrl(url);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +29,7 @@ class DataView extends StatelessWidget {
             itemBuilder: (context, index) {
               final data = response.items?[index];
               final avatarUrl = data?.owner?.avatarUrl;
+              final pageUrl = data?.htmlUrl;
 
               return ListTile(
                 leading: (avatarUrl != null)
@@ -32,10 +38,13 @@ class DataView extends StatelessWidget {
                       )
                     : null,
                 title: Text(data?.name ?? '----'),
-                subtitle: Text(data?.description ?? '----'),
-                onTap: () {
-                  // Handle tile tap
-                },
+                subtitle: Text(
+                  data?.description ?? '----',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                onTap: () =>
+                    pageUrl != null ? launch(Uri.parse(pageUrl)) : null,
               );
             },
           ),
